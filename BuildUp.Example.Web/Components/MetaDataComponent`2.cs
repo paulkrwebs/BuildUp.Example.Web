74f5fc5a4ef7052f7c0c0a1db6ff1ddc2f753c79
Paul Kiernan <paulk@rwebs.co.uk>
2016-10-02 14:11:07 +0100

    removed unused usings

--- BuildUp.Example.Web/Components/MetaDataComponent`2.cs
+++ BuildUp.Example.Web/Components/MetaDataComponent`2.cs
@@ -1,12 +1,7 @@
 ﻿namespace BuildUp.Example.Web.Components
 {
-    using AutoMapper;
     using BuildUp.Example.Web.ViewModels.Pages;
-    using ContentModels;
     using Example.ContentModels;
-    using System;
-    using System.Collections.Generic;
-    using ViewModels;
 
     public class MetaDataComponent<TToCreate, TFrom> :
         IComponent<ComponentArgs<TToCreate, TFrom>>
